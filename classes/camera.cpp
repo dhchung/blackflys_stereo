72b82f1e90da7234a2854ff98475e3fd753d6852
morin <morin@kaist.ac.kr>
2021-05-05 13:04:40 +0900

    asdf

--- classes/camera.cpp
+++ classes/camera.cpp
@@ -1,5 +1,4 @@
 #include "camera.h"
-#include <chrono>
 
 
 Camera::Camera(){
@@ -18,6 +17,8 @@ Camera::~Camera(){
     if(camera_ready){
         cam_1->EndAcquisition();
         cam_2->EndAcquisition();
+        cam_1->DeInit();
+        cam_2->DeInit();
     }
     camList.Clear();
 
@@ -43,11 +44,20 @@ void Camera::set_camera(){
         camera_ready = true;
         cam_1 = camList.GetBySerial(cam_1_serial);
         cam_2 = camList.GetBySerial(cam_2_serial);
+
+        cam_1->DeInit();
+        cam_2->DeInit();
+
+
         cam_1->Init();
+
+
+
         cam_1->LineSelector.SetValue(Spinnaker::LineSelector_Line2);
         cam_1->V3_3Enable.SetValue(true);
 
         cam_2->Init();
+
         cam_2->TriggerMode.SetValue(Spinnaker::TriggerMode_Off);
         cam_2->TriggerSource.SetValue(Spinnaker::TriggerSource_Line3);
         cam_2->TriggerOverlap.SetValue(Spinnaker::TriggerOverlap_ReadOut);
@@ -55,6 +65,19 @@ void Camera::set_camera(){
 
         cam_1->AcquisitionMode.SetValue(Spinnaker::AcquisitionMode_Continuous);
         cam_2->AcquisitionMode.SetValue(Spinnaker::AcquisitionMode_Continuous);
+
+        if(cam_1->DecimationVertical.GetValue()!= 2){
+            cam_1->DecimationVertical.SetValue(2);
+        }
+        if(cam_1->DecimationHorizontal.GetValue()!= 2){
+            cam_1->DecimationHorizontal.SetValue(2);
+        }
+        if(cam_2->DecimationVertical.GetValue()!= 2){
+            cam_2->DecimationVertical.SetValue(2);
+        }
+        if(cam_2->DecimationHorizontal.GetValue()!= 2){
+            cam_2->DecimationHorizontal.SetValue(2);
+        }
 
         Spinnaker::GenApi::INodeMap& sNodeMap_1 = cam_1->GetTLStreamNodeMap();
         Spinnaker::GenApi::INodeMap& sNodeMap_2 = cam_2->GetTLStreamNodeMap();
@@ -85,6 +108,7 @@ void Camera::set_camera(){
         }
 
 
+
         ptrHandlingModeEntry_1 = ptrHandlingMode_1->GetEntryByName("NewestFirst");
         ptrHandlingMode_1->SetIntValue(ptrHandlingModeEntry_1->GetValue());
         std::cout << std::endl
@@ -113,12 +137,10 @@ void Camera::set_camera(){
 
 
 std::vector<cv::Mat> Camera::acquire_image(){
-    auto t0 = std::chrono::high_resolution_clock::now();
 
     Spinnaker::ImagePtr img1 = cam_1->GetNextImage();
     Spinnaker::ImagePtr img2 = cam_2->GetNextImage();
 
-    auto t1 = std::chrono::high_resolution_clock::now();
 
     std::vector<cv::Mat> image_vector;
     image_vector.resize(2);
@@ -138,23 +160,22 @@ std::vector<cv::Mat> Camera::acquire_image(){
     const size_t height_1 = img1->GetHeight();
     Spinnaker::ImagePtr convertedImage_1 = img1->Convert(Spinnaker::PixelFormat_BGR8, Spinnaker::BILINEAR);
     cv::Mat imgMat_1 = cv::Mat(cv::Size(width_1, height_1), CV_8UC3, convertedImage_1->GetData());
-    cv::resize(imgMat_1, imgMat_1, cv::Size(imgMat_1.cols/6, imgMat_1.rows/6));
+    cv::resize(imgMat_1, imgMat_1, cv::Size(imgMat_1.cols/1, imgMat_1.rows/1));
 
 
     const size_t width_2 = img2->GetWidth();
     const size_t height_2 = img2->GetHeight();
     Spinnaker::ImagePtr convertedImage_2 = img2->Convert(Spinnaker::PixelFormat_BGR8, Spinnaker::BILINEAR);
     cv::Mat imgMat_2 = cv::Mat(cv::Size(width_2, height_2), CV_8UC3, convertedImage_2->GetData());
-    cv::resize(imgMat_2, imgMat_2, cv::Size(imgMat_2.cols/6, imgMat_2.rows/6));
+    cv::resize(imgMat_2, imgMat_2, cv::Size(imgMat_2.cols/2, imgMat_2.rows/2));
 
 
     image_vector[0] = imgMat_1;
     image_vector[1] = imgMat_2;
 
-    auto t2 = std::chrono::high_resolution_clock::now();
 
-    cv::Mat concat;
-    cv::hconcat(imgMat_1, imgMat_2, concat);
+    // cv::Mat concat;
+    // cv::hconcat(imgMat_1, imgMat_2, concat);
     // cv::resize(concat, concat, cv::Size(concat.cols/6, concat.rows/6));
     // cv::imshow("Fuck", concat);
     // cv::waitKey(1);
@@ -162,11 +183,6 @@ std::vector<cv::Mat> Camera::acquire_image(){
     img1->Release();
     img2->Release();
 
-    auto t01 = 1.e-3*std::chrono::duration_cast<std::chrono::milliseconds>(t1-t0).count();
-    auto t12 = 1.e-3*std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count();
-
-    std::cout<<"t01 : "<<t01<<std::endl;
-    std::cout<<"t12 : "<<t12<<std::endl;
 
 
     return image_vector;
